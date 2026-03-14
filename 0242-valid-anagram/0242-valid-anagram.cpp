02231869a80e01f660edad829c32ca4410ce499e
botluecifer <singhakarshmmmut@gmail.com>
2026-03-14 21:52:41 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.86%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        int m = s.size();
+        int n = t.size();
+
+        if(m != n){
+            return false;
+        }
+
+        vector<int> count(26,0);
+
+        for(int i = 0; i < m; i++){
+            count[s[i] - 'a']++;
+        }
+
+        for(int i = 0; i < n; i++){
+            count[t[i] - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
